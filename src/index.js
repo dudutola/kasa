6d4c51f976821b367c0165c6405543049f9b6c51
dulcelene machado <dudutola@gmail.com>
2024-03-16 11:39:41 +0100

    Added an error component to dispplay an error message when the route does not exist Included the route in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Error from './components/Error';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={ <Home /> } />
+        <Route path='*' element={ <Error /> } />
       </Routes>
     </Router>
   </React.StrictMode>,
