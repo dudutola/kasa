0c246e7facef541b312d3f804936631d1f1de3ce
dulcelene machado <dudutola@gmail.com>
2024-03-22 16:13:42 +0100

    Removed the banner from home and added it in a new Banner component, and called it in Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "../../styles/pages/_home.scss";
 import "../../styles/components/_banner.scss";
-import BannerImage from "../../assets/Image source 1.png";
+import Banner from "../../components/Banner";
 
 function Home() {
   return (
-    <div className="banner">
-      <div className="banner-image">
-        <img src={BannerImage} alt="Image source" />
-      </div>
-      <div className="banner-title">
-        <h1>Chez vous, partout et ailleurs</h1>
-      </div>
+    <div className="container">
+      <Banner />
     </div>
   );
 }
