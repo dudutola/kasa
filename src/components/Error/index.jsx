3120bb73e6a90d42f011d624bbf17045198aca95
dulcelene machado <dudutola@gmail.com>
2024-03-28 12:39:33 +0100

    Updated Error page to display a 404 error message with a link to return to the homepage, styled using SCSS Added mobile and tablet responsive also

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ErrorMessage from "../../assets/404.png";
+import "../../styles/components/_error.scss";
 
 function Error() {
   return (
-    <div>
-      <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+    <div className="error">
+      <img src={ErrorMessage} alt="Error message 404" className="error__img" />
+      <h1 className="error__message">Oups! La page que<br className="error__mobile-only"/> vous demandez n'existe pas.</h1>
+      <Link to='/' className="error__back">Retourner sur la page d’accueil</Link>
     </div>
   )
 }
