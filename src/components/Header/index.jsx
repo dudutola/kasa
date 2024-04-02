bc7234e0195ab6bb33ba682dd11d320cb8f88740
dulcelene machado <dudutola@gmail.com>
2024-04-02 12:43:19 +0200

    Updated the links to only be underlined when the location is either accueil or a propos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/LOGO.png";
 import '../../styles/layout/_header.scss'
 
 function Header() {
+  const location = useLocation()
+  const [activeLink, setActiveLink] = useState('')
+
+  useEffect(() => {
+    const pathname = location.pathname
+
+    if (pathname === "/") {
+      setActiveLink("homepage")
+    } else if (pathname === "/about") {
+      setActiveLink("about")
+    } else {
+      setActiveLink("")
+    }
+  }, [location])
+
   return (
     <nav className="navbar">
       <Link to='/'><img src={Logo} alt="Logo Kasa" width={143.32} height={46}/></Link>
       <div className="navbar__link">
-        <Link to='/'>Accueil</Link>
-        <Link to='/about'>A Propos</Link>
+        <Link to='/' className={activeLink === 'homepage' ? 'active' : ''}>Accueil</Link>
+        <Link to='/about' className={activeLink === 'about' ? 'active' : ''}>A Propos</Link>
       </div>
     </nav>
   )
